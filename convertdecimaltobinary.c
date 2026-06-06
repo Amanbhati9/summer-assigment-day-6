8cad9de04c3648b7c708697593ecaaeb404e183e
Amanbhati9 <bhatiaman699@gmail.com>
2026-06-06 21:59:02 +0530

    second question

---
+++ convertdecimaltobinary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void toBinary(int n) {
+    int binary[32];
+    int i = 0;
+
+    while (n > 0) {
+        binary[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    printf("Binary: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+
+    if (n == 0) {
+        printf("Binary: 0");
+    } else {
+        toBinary(n);
+    }
+
+    return 0;
+}
